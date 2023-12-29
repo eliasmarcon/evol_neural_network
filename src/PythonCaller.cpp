0c3d07529e143baabac519335f66fb3e8ac9b700
eliasmarcon <elias.marcon8@gmail.com>
2023-12-29 15:22:34 +0100

    Co-authored-by: janpilu <janpilu@users.noreply.github.com>

---
+++ src/PythonCaller.cpp
@@ -0,0 +1,35 @@
+// PythonCaller.cpp
+#include "PythonCaller.h"
+
+PyObject* PythonCaller::CallPythonFunction(const std::string &moduleName, 
+                                           const std::string &functionName, 
+                                           PyObject *args) {
+    Py_Initialize();
+    PyRun_SimpleString("import sys");
+    PyRun_SimpleString("sys.path.append('.')");
+
+    PyObject *pName = PyUnicode_FromString(moduleName.c_str());
+    PyObject *pModule = PyImport_Import(pName);
+    Py_DECREF(pName);
+    PyObject *pResult = nullptr;
+
+    if (pModule != nullptr) {
+        PyObject *pFunc = PyObject_GetAttrString(pModule, functionName.c_str());
+        if (pFunc && PyCallable_Check(pFunc)) {
+            pResult = PyObject_CallObject(pFunc, args);
+            if (pResult == nullptr) {
+                PyErr_Print();
+            }
+        } else {
+            if (PyErr_Occurred())
+                PyErr_Print();
+        }
+        Py_XDECREF(pFunc);
+        Py_DECREF(pModule);
+    } else {
+        PyErr_Print();
+    }
+
+    Py_Finalize();
+    return pResult;
+}
---
+++ src/example/array_processor.py
@@ -0,0 +1,3 @@
+# array_processor.py
+def process_array(arr):
+    return [x * 2 for x in arr]  # Example processing: double each element

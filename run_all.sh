d392cc0262b89402ec44f63cce928e43525d7f29
squirtlemon <elias.marcon8@gmail.com>
2024-01-12 16:24:13 +0100

    clean up

--- run_all.sh
+++ run_all.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo "Building the Docker image..."
+echo -e "\nBuilding the Docker image...\n"
 # Build the Docker image
 docker build -t neural_network .
 
 
-echo "Running the Docker container..."
+echo -e "\nRunning the Docker container...\n"
 # Run the Docker container and mount the current directory
 docker run -v "$(pwd):/app/" neural_network
 
 
-echo "Copying the output file from the Docker container to the host..."
+echo -e "\nCopying the output file from the Docker container to the host...\n"
 # Get the Docker container ID
 DOCKER_ID=$(docker ps -lq)
 # Copy the output file from the Docker container to the host
@@ -19,9 +19,9 @@ docker cp "$DOCKER_ID:/app/neural_network_output.txt" ./neural_network_output.tx
 
 # Check for the number of arguments and if the argument is "no"
 if [ "$#" -eq 1 ] && [ "$1" == "no" ]; then
-    echo "Not deleting the Docker container."
+    echo -e "\nNot deleting the Docker container.\n"
 else
-    echo "Deleting the Docker container..."
+    echo -e "\nDeleting the Docker container..."
     # Delete the Docker container
     docker rm "$DOCKER_ID"
 fi

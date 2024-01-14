96500918eaf23d22061c05024205ca1ffbe224f6
squirtlemon <elias.marcon8@gmail.com>
2024-01-14 17:33:21 +0100

    tested docker image, adapted powershell files

--- run_all_prebuilt.sh
+++ run_all_prebuilt.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
 
+# Function to check if a given value is an integer
+is_integer() {
+    [[ "$1" =~ ^[0-9]+$ ]]
+}
+
+# Check for the number of arguments
+if [ "$#" -eq 2 ]; then
+    if is_integer "$1" && is_integer "$2"; then
+        populationSize=$1
+        generations=$2
+        deleteDocker="yes"
+    else
+        echo "Error: populationSize and generations must be integers."
+        exit 1
+    fi
+elif [ "$#" -eq 3 ]; then
+    if is_integer "$1" && is_integer "$2" && [ "$3" == "yes" -o "$3" == "no" ]; then
+        populationSize=$1
+        generations=$2
+        deleteDocker=$3
+    else
+        echo "Error: populationSize and generations must be integers, and deleteDocker must be 'yes' or 'no'."
+        exit 1
+    fi
+else
+    echo "Usage: ./run_all.sh <populationSize> <generations> <deleteDocker>"
+    exit 1
+fi
+
+
 echo -e "\nRunning the Docker container...\n"
 # Run the Docker container and mount the current directory
-docker run -v "$(pwd):/app/" fasteiner/ubuntu-galib-neural-network
+docker run -v "$(pwd):/app/" -e POPULATION_SIZE="$populationSize" -e GENERATIONS="$generations" fasteiner/ubuntu-galib-neural-network
 
 
 echo -e "\nCopying the output file from the Docker container to the host...\n"
@@ -13,7 +43,7 @@ docker cp "$DOCKER_ID:/app/neural_network_output.txt" ./neural_network_output.tx
 
 
 # Check for the number of arguments and if the argument is "no"
-if [ "$#" -eq 1 ] && [ "$1" == "no" ]; then
+if [ "$deleteDocker" == "no" ]; then
     echo -e "\nNot deleting the Docker container.\n"
 else
     echo -e "\nDeleting the Docker container..."
